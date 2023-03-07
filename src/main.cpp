6ffa6fb3564ba7896fc7fb3213949afff2db691f
brentru <brent@adafruit.com>
2023-03-07 16:48:58 -0500

    working towards brighter screen but have black bg

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,8 @@
 #include <lvgl.h>
 #include <Adafruit_ST7789.h>
 
-extern lv_font_t turtle;
+//extern lv_font_t turtle;
+extern lv_font_t turtle2;
 #define TURTLE_SYMBOL "\xEF\x9C\xA6"
 
 #define BIRD_SYMBOL "\xEE\x91\xA9"
@@ -21,18 +22,24 @@ Adafruit_LvGL_Glue glue;
 // function rather than in setup(), so simple programs can just
 // copy-and-paste this sketch as a starting point, then embellish here:
 void lvgl_setup(void) {
+ // make background black
+ lv_obj_set_style_bg_color(lv_scr_act(), LV_COLOR_MAKE(0, 0, 0), LV_STATE_DEFAULT);
 
  lv_obj_t *label = lv_label_create(lv_scr_act());
 
   static lv_style_t style;
   lv_style_init(&style);
-  lv_style_set_text_font(&style, &turtle);
+  //lv_style_set_text_font(&style, &turtle);
+  lv_style_set_text_font(&style, &turtle2);
   lv_obj_add_style(label, &style, LV_PART_MAIN);
 
   // Turtle font
   lv_label_set_text(label, TURTLE_SYMBOL);
-  lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+  lv_obj_align(label, LV_ALIGN_TOP_LEFT, 0, 0);
 
+  lv_obj_t *labelAIO = lv_label_create(lv_scr_act());
+  lv_label_set_text(labelAIO, "WipperSnapper");
+  lv_obj_align(labelAIO, LV_ALIGN_TOP_MID, 0, 0);
 }
 
 void setup(void) {
