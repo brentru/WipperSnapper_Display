a1509b8fba4d1f0d20a3ab35a7bf5dc28af7df0b
brentru <brent@adafruit.com>
2023-03-21 15:07:32 -0400

    small refactor, documentation added

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,11 @@
 #include <Adafruit_ST7789.h>
 #include <lvgl.h>
 
+// adafruit_lvgl_glue
+Adafruit_ST7789 tft(TFT_CS, TFT_DC, TFT_RESET);
+Adafruit_LvGL_Glue glue;
+
+// lvgl
 // FA symbol glyphs
 #define SYMBOL_CODE "\xEF\x87\x89"
 #define SYMBOL_WIFI "\xEF\x87\xAB"
@@ -17,50 +22,50 @@
 #define SYMBOL_CHECKMARK "\xEF\x81\x98"
 #define SYMBOL_ERROR_TRIANGLE "\xEF\x81\xB1"
 
-// images
-LV_IMG_DECLARE(ws_icon_100px);
-LV_IMG_DECLARE(ws_logo_200px);
-
 // FA symbol properties
 extern lv_font_t errorTriangle, circle_30px, cloud_30px, turtle_30px, wifi_30px,
     file_code;
 
-Adafruit_ST7789 tft(TFT_CS, TFT_DC, TFT_RESET);
-Adafruit_LvGL_Glue glue;
+// images
+LV_IMG_DECLARE(ws_icon_100px);
+LV_IMG_DECLARE(ws_logo_200px);
 
-// Styles used by the loading screen, need to be global or static
+// splash screen image
+static lv_obj_t *splashImg;
+
+// loading bar icons
+static lv_obj_t *lblIconFile, *lblIconWiFi;
+
+// styles used by the loading screen, need to be global or static
 static lv_style_t styleIconFile, styleIconWiFi, styleIconTurtle30px,
     styleIconCloud, styleIconCheckmark;
 
+/* Logger for USB serial */
 void my_log_cb(const char *buf) { Serial.printf(buf); }
-
-lv_timer_t *splashTask;
-lv_obj_t *splashImg;
-
-lv_obj_t *labelIconFile, *labelWiFi;
 
 void make_wifi_green(lv_timer_t *timer) {
   lv_style_set_text_color(&styleIconWiFi, lv_palette_main(LV_PALETTE_GREEN));
-  lv_obj_add_style(labelWiFi, &styleIconWiFi, LV_PART_MAIN);
+  lv_obj_add_style(lblIconWiFi, &styleIconWiFi, LV_PART_MAIN);
   lv_timer_del(timer);
 }
 
 void make_file_green(lv_timer_t *timer) {
   Serial.printf("make_file_green()");
   lv_style_set_text_color(&styleIconFile, lv_palette_main(LV_PALETTE_GREEN));
-  lv_obj_add_style(labelIconFile, &styleIconFile, LV_PART_MAIN);
+  lv_obj_add_style(lblIconFile, &styleIconFile, LV_PART_MAIN);
 
   lv_timer_del(timer);
 }
 
-void load_task(lv_timer_t *timer) {
-  Serial.printf("load_task()");
+/* Dynamically builds and shows the loading screen */
+void timer_build_loader_cb(lv_timer_t *timer) {
+  Serial.printf("timer_build_loader_cb()");
   // clear out the splash screen
-  Serial.printf("load_task(): clearing splash screen");
+  Serial.printf("timer_build_loader_cb(): clearing splash screen");
   // remove the splash screen (TODO: do this in another func.)
   lv_obj_del(splashImg);
 
-  Serial.printf("load_task(): building load screen");
+  Serial.printf("timer_build_loader_cb(): building load screen");
   // adding loading screen image
   lv_obj_t *icon = lv_img_create(lv_scr_act());
   lv_img_set_src(icon, &ws_icon_100px);
@@ -72,24 +77,24 @@ void load_task(lv_timer_t *timer) {
   const int iconBarXSpaces = 33;         // +10 exactly between icons
 
   // add symbol_code (30px) to represent settings.json
-  labelIconFile = lv_label_create(lv_scr_act());
-  lv_label_set_text(labelIconFile, SYMBOL_CODE);
+  lblIconFile = lv_label_create(lv_scr_act());
+  lv_label_set_text(lblIconFile, SYMBOL_CODE);
   // formatting
   lv_style_init(&styleIconFile);
   lv_style_set_text_color(&styleIconFile, lv_palette_main(LV_PALETTE_GREY));
   lv_style_set_text_font(&styleIconFile, &file_code);
-  lv_obj_add_style(labelIconFile, &styleIconFile, LV_PART_MAIN);
-  lv_obj_align(labelIconFile, LV_ALIGN_BOTTOM_LEFT, iconBarXStart,
+  lv_obj_add_style(lblIconFile, &styleIconFile, LV_PART_MAIN);
+  lv_obj_align(lblIconFile, LV_ALIGN_BOTTOM_LEFT, iconBarXStart,
                iconBarYOffset);
 
   // add symbol_wifi (30px) to represent wifi connect
-  labelWiFi = lv_label_create(lv_scr_act());
-  lv_label_set_text(labelWiFi, SYMBOL_WIFI);
+  lblIconWiFi = lv_label_create(lv_scr_act());
+  lv_label_set_text(lblIconWiFi, SYMBOL_WIFI);
   lv_style_init(&styleIconWiFi);
   lv_style_set_text_color(&styleIconWiFi, lv_palette_main(LV_PALETTE_GREY));
   lv_style_set_text_font(&styleIconWiFi, &wifi_30px);
-  lv_obj_add_style(labelWiFi, &styleIconWiFi, LV_PART_MAIN);
-  lv_obj_align(labelWiFi, LV_ALIGN_BOTTOM_LEFT,
+  lv_obj_add_style(lblIconWiFi, &styleIconWiFi, LV_PART_MAIN);
+  lv_obj_align(lblIconWiFi, LV_ALIGN_BOTTOM_LEFT,
                iconBarXStart + (iconBarXSpaces * 1), iconBarYOffset);
 
   // Add symbol turtle 30px
@@ -126,26 +131,27 @@ void load_task(lv_timer_t *timer) {
   lv_obj_add_style(labelCircleBar, &styleIconCheckmark, LV_PART_MAIN);
   lv_obj_align(labelCircleBar, LV_ALIGN_BOTTOM_LEFT, 160 + 33, iconBarYOffset);
 
-  Serial.printf("load_task(): built load screen!");
-  // TODO: Next task should check for secrets.json
-  lv_timer_create(make_file_green, 3000, NULL);
+  Serial.printf("timer_build_loader_cb(): built load screen!");
 
+  // delete this timer to prevent a repeat run
   lv_timer_del(timer);
+
+  // TODO: Next task should check for secrets.json instead
+  lv_timer_create(make_file_green, 3000, NULL);
 }
 
-void splash_task(lv_timer_t *timer) {
-  Serial.printf("splash_task()");
-  // create and load splash screen
-
-  // create and center the full logo (200px
+void timer_splash_cb(lv_timer_t *timer) {
+  Serial.printf("timer_splash_cb()");
+  // build and show the splash screen
   splashImg = lv_img_create(lv_scr_act());
   lv_img_set_src(splashImg, &ws_logo_200px);
   lv_obj_align(splashImg, LV_ALIGN_CENTER, 0, 0);
 
-  lv_timer_del(timer); // one-shot task, delete
+  // delete this timer to prevent a repeat run
+  lv_timer_del(timer);
 
-  // add load_task to timer handler
-  lv_timer_create(load_task, 5000, NULL);
+  // next, loading bar executes after 2.5sec spent within the current timer
+  lv_timer_create(timer_build_loader_cb, 2500, NULL);
 }
 
 void setup(void) {
@@ -167,10 +173,11 @@ void setup(void) {
 
   lv_log_register_print_cb(my_log_cb);
 
+  // set screen background to black
   lv_obj_set_style_bg_color(lv_scr_act(), lv_color_black(), LV_STATE_DEFAULT);
-  // task 1
-  Serial.printf("Setup");
-  splashTask = lv_timer_create(splash_task, 0, NULL);
+
+  // task 1 - splash screen launches immediately
+  lv_timer_create(timer_splash_cb, 0, NULL);
 }
 
 void loop(void) {
